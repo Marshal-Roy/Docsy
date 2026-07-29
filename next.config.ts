0facc97d489aeb06e908f3b4c27e93e6eb333c8e
Marshal <marshalroy911@gmail.com>
2026-07-29 14:27:26 +0530

    compression fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ['compress-pdf'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/compress': ['./node_modules/compress-pdf/bin/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
